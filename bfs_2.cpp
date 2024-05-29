532ff3ad49501d2c2c6b284d578ea08bca740d97
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2024-05-30 00:25:52 +0530

    problem 1 solution - issue of wrong output of traversal

--- bfs_2.cpp
+++ bfs_2.cpp
@@ -1,28 +1,24 @@
-// optimization 1:
-// 1. using adjacency list (memory efficient for sparse matrix)
-// 2. 2 queues (for managing BFS levels)
-// 3. #pragma omp parallel , #pragma omp for to paralelize the processing of nodes at current level.
-// (every thread processes a subset of nodes and collects the nodes for next level in thread-local set to avoid race conditions)
-// 4. synchronisation- #pragma omp critical (to safely update data structures)
+//code for solving problems in optimization 1:
 
-// achieved - can process large graphs because of using chunk_size
-// PROBLEM1- order in which nodes enqueued and dequeued in the parallel  region (hence wrong traversal even in small graph of 5 edges)
-// PROBLEM2- large number of 1's printed at the end of output
-// due to :improper handling of the queue and synchronization in the OpenMP parallel region. 
-// Specifically, the parallel processing of the queue and the use of #pragma omp critical not be managing the state of the currentLevel queue correctly.
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <omp.h>
-#include <unordered_set>
+// PROBLEM1- wrong traversal in small graph of 5 nodes:
+// solution - using mutex lock to manage concurrent access.
+// persisting problem in this solution- still there are trailing 1's in the end of traversal output when we input large graphs 
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<omp.h>
+#include<mutex>
+#include<unordered_set>
 
 using namespace std;
 
 class BFS {
     vector<vector<int>> adjList; // adjacency list
     int numNodes;
+
 
 public:
     BFS(int numNodes) : numNodes(numNodes) //initializer list
@@ -45,61 +41,52 @@ void BFS::input(const vector<pair<int, int>>& edges) {
 void BFS::bfs(int startVertex) {
     vector<bool> visited(numNodes, false);
     queue<int> currentLevel, nextLevel; 
-
+    mutex queueMutex;
     currentLevel.push(startVertex); //pushing start vertex on the queue currentLevel and marking it as visited
     visited[startVertex] = true;
 
     while (!currentLevel.empty()) {
-        #pragma omp parallel
-        {
-            unordered_set<int> localNextLevel;
+        vector<int>temp_currentLevel;
 
-            #pragma omp for
-            for (int i = 0; i < currentLevel.size(); ++i) {
-                int v;
+        // extracting all nodes from current level
+        while(!currentLevel.empty()){
+            temp_currentLevel.push_back(currentLevel.front());
+            currentLevel.pop();
+        }
+
+            #pragma omp parallel for
+            for (int i = 0; i < temp_currentLevel.size(); ++i) {
+                int v = temp_currentLevel[i];
                 #pragma omp critical
                 {
-                    if (!currentLevel.empty()) {
-                        v = currentLevel.front();
-                        currentLevel.pop();
-                    } else {
-                        continue;
-                    }
+                    cout << v << " ";
                 }
 
-                cout << v << " ";
 
+            // processing all the adjacent nodes
                 for (int neighbor : adjList[v]) {
                     if (!visited[neighbor]) {
                         #pragma omp critical
                         {
                             if (!visited[neighbor]) {
                                 visited[neighbor] = true;
-                                localNextLevel.insert(neighbor);
+                                queueMutex.lock();
+                                nextLevel.push(neighbor);
+                                queueMutex.unlock();
                             }
                         }
                     }
                 }
             }
 
-            #pragma omp critical
-            {
-                for (int node : localNextLevel) {
-                    nextLevel.push(node);
-                }
-            }
-        }
 
         swap(currentLevel, nextLevel);
-        while (!nextLevel.empty()) {
-            nextLevel.pop();
-        }
     }
 }
 
 int main() {
     // Opening file to read edges
-    ifstream infile("com-youtube.ungraph.txt");
+    ifstream infile("edgelist.txt");
     if (!infile) {
         cerr << "File not found";
         return 1;
@@ -135,7 +122,7 @@ int main() {
     if (!edges.empty()) {
         BFS obj(maxNodeId + 1); // Create BFS object with maximum node ID seen
         obj.input(edges);
-        obj.bfs(startNode); // Assuming starting BFS from node 0
+        obj.bfs(startNode); 
     }
 
     return 0;
